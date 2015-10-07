e35650f5141e83cf936ae708aa19dc38a99f010e
s2it-globo <renato.oliveira@s2it.com.br>
2015-10-07 18:02:35 -0300

    Update set_salt.sh

--- set_salt.sh
+++ set_salt.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 salt="$(/usr/bin/bbb-conf --salt|grep Salt|cut -c13-44)" 
-sed -i "s|bced839c079b4fe2543aefa73c7f6a57|$salt|" $HOME/dev/bigbluebutton-0.9.1/bigbluebutton-meeting/src/main/webapp/bbb_api_conf.jsp
+sed -i "s|bced839c079b4fe2543aefa73c7f6a57|$salt|" $HOME/dev/bigbluebutton/bigbluebutton-meeting/src/main/webapp/bbb_api_conf.jsp
 
