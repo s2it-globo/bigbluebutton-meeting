c353e41f60dc5efb6b6a48f5f5517a5f2a5eb809
Jotagê Sales <jotage_sales@hotmail.com>
2015-10-16 19:00:30 -0300

    create sh for generate certs

---
+++ generate-cert.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+sed -i '8i SAN="email:jose.sales@s2it.com.br"' /usr/lib/ssl/openssl.cnf
+
+sed -i '219i subjectAltName=${ENV::SAN}' /usr/lib/ssl/openssl.cnf
+
+sed -i '226i subjectAltName=${ENV::SAN}' /usr/lib/ssl/openssl.cnf
+
+export SAN="IP:$1"
+
+rm /etc/nginx/ssl/bigbluebutton.crm bigbluebutton.key
+
+#gerando certificado ja com todos os parametros
+/usr/bin/openssl req -x509 -sha256 -nodes -days 365 -newkey rsa:2048 -subj '/CN=BigBlueButton/O=BigBlueButton, Inc./C=US/ST=Oregon/L=Portland' -nodes -out apache.pem -keyout apache.key
+
